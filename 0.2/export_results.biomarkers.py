9ebfefed835463fdc92443fc7b7bd9e7c9b7576f
Liam Abbott <liam.e.abbott@gmail.com>
2019-05-13 17:22:26 -0400

    rebase

--- 0.2/export_results.biomarkers.py
+++
@@ -1,9 +0,0 @@
-ht = hl.read_table(path)
-for i, y in enumerate(phenotypes):
-    ht_out = ht.select(
-        variant=hl.delimit([
-            ht['locus'].contig,
-            ht['locus'].position,
-            ht['alleles'][0],
-            ht['alleles'][1]], delimiter=':'),
-        )
